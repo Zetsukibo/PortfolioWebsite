1c66db71e340f29b2c3fda9f64357b431725538f
Mark017-CS <advinculamarkangelo@gmail.com>
2023-05-16 15:19:50 +0800

    logo added

--- Home/portfolio.php
+++ Home/portfolio.php
@@ -68,8 +68,8 @@ $user_data = mysqli_fetch_array($run);
 
 
     <div class="container">
-    <!--  <img src="../images/logo.png" class="img-fluid" alt="" style="width: 4vw; cursor: pointer; font-style: italic; margin-top: -190px;margin-bottom: 150px; margin-left: -100px; "> -->
-      <a href="./home.html" class="logo" style=" display: flex; align-items: center; text-decoration: none; font-size: 1.5rem; color: var(--text-color); margin-top: -190px;margin-bottom: 180px; margin-left: -40px; font-style: italic; font-weight: 600; cursor: pointer;"><b style="color: #1DB954;">Art </b><b style="color: #FFF;">Abode</b></a>
+    <img src="../images/logo.png" class="img-fluid" alt="" style="width: 4vw; cursor: pointer; font-style: italic; margin-top: -190px;margin-bottom: 150px; margin-left: -100px; "> 
+      <a href="./home.html" class="logo" style=" display: flex; align-items: center; text-decoration: none; font-size: 1.5rem; color: var(--text-color); margin-top: -200px;margin-bottom: 160px; margin-left: -40px; font-style: italic; font-weight: 600; cursor: pointer;"><b style="color: #1DB954;">rt </b><b style="color: #FFF;">Abode</b></a>
       <h1><a href="portfolio.php">
           <?= $user_data['title'] ?>
         </a></h1>
